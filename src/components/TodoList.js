b4eb6f5b43b7a9ad3c6f6e1b5cf3013157b224ee
Daniela Brailovsky <dbrailo@gmail.com>
2022-02-15 13:13:23 +0200

    Added functions

---
+++ src/components/TodoList.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>What would you like to add?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} />
+    </div>
+  );
+}
+
+export default TodoList;
